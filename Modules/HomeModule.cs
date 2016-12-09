5c46d73b0ce22fec0710f6d79ed6b43225c65a17
bradcopenhaver <bradcopenhaver@gmail.com>
2016-12-09 14:40:25 -0800

    create views and routes for homepage and new stylist creation

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -9,7 +9,19 @@ namespace HairSalon
   {
     public HomeModule()
     {
-      
+      Get["/"] = _ => {
+        List<Stylist> allStylists = Stylist.GetAll();
+        return View["index.cshtml", allStylists];
+      };
+      Get["/stylist/add"] = _ => {
+        return View["new-stylist-form.cshtml"];
+      };
+      Post["/stylist/add"] = _ => {
+        Stylist newStylist = new Stylist(Request.Form["stylistName"]);
+        newStylist.Save();
+        List<Stylist> allStylists = Stylist.GetAll();
+        return View["index.cshtml", allStylists];
+      };
     }
   }
 }
